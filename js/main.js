457682ba02b4f923e08c3b3d261ef746487a7892
Paul Grosen <pcgrosen@cs.ucsb.edu>
2018-02-15 11:13:04 -0800

    initial support for Boards

--- js/main.js
+++ js/main.js
@@ -24,6 +24,10 @@ if (typeof(localStorage.analogPins) !== "undefined") {
   }
 }
 
+document.getElementById("name").value = (typeof(localStorage.name) === "undefined") ? "" : localStorage.name;
+document.getElementById("exercise-number").value = (typeof(localStorage.exerciseNumber) === "undefined") ? "" : localStorage.exerciseNumber;
+document.getElementById("board").value = (typeof(localStorage.board) === "undefined") ? "" : localStorage.board;
+
 editor.setValue(code);
 editor.focus();
 
@@ -32,9 +36,6 @@ editor.commands.addCommand({
    bindKey: {win: "Ctrl-Enter", mac: "Ctrl-Enter"},
    exec: runCode
 });
-
-document.getElementById("name").value = (typeof(localStorage.name) === "undefined") ? "" : localStorage.name;
-document.getElementById("exercise-number").value = (typeof(localStorage.exerciseNumber) === "undefined") ? "" : localStorage.exerciseNumber;
 
 new Clipboard("#copy-page", {
   text: function() {
@@ -144,24 +145,6 @@ function loadFile(file) {
 
 var running = false;
 var lastContent = {frameManager: null, output: null};
-
-var ledLookup = {
-  2: {x: 87, y: 165, color: "red"},
-  3: {x: 87, y: 140, color: "green"},
-  4: {x: 87, y: 115, color: "red"},
-  5: {x: 87, y: 90, color: "green"},
-  6: {x: 87, y: 65, color: "red"},
-  7: {x: 87, y: 35, color: "green"},
-  8: {x: 87, y: 10, color: "red"},
-
-  9: {x: 5, y: 165, color: "orange"},
-  10: {x: 5, y: 140, color: "orange"},
-  11: {x: 5, y: 115, color: "orange"},
-  12: {x: 5, y: 90, color: "blue"},
-  13: {x: 5, y: 65, color: "blue"},
-  14: {x: 5, y: 35, color: "yellow"},
-  15: {x: 5, y: 7, color: "yellow"}
-};
 
 function frameManagerFromJSON(string) {
   var data = JSON.parse(string);
@@ -367,10 +350,6 @@ function generateGif(frameManager, isCorrect) {
   var gifOutput = document.getElementById("gif-output");
   gifOutput.innerHTML = "Generating gif . . .";
 
-  var canvas = document.createElement("canvas");
-  canvas.height = 195;
-  canvas.width = 300;
-
   var gif = new GIF({workers: 4, quality: 10, workerScript: "js/gif/gif.worker.js", transparent: 0xFFFFFF, width: canvas.width, height: canvas.height});
   var ctx = canvas.getContext("2d");
 
@@ -381,20 +360,24 @@ function generateGif(frameManager, isCorrect) {
     return;
   }
 
-  var on_finished = function(gif, e) {
+  var board = new BOARDS[document.getElementById("board").value]();
+
+  var canvas = document.createElement("canvas");
+  canvas.height = board.canvasHeight;
+  canvas.width = board.canvasWidth;
+
+  var onFinished = function(gif, e) {
     gifOutput.innerHTML = "";
     var img = document.createElement("img");
     var binString = "";
     e.forEach(function (element) {
       binString += String.fromCharCode(element);
     });
-    img.src = "data:image/gif;base64," + btoa(binString); //+ btoa(String.fromCharCode.apply(null, e));
+    img.src = "data:image/gif;base64," + btoa(binString);
     img.id = "output-image";
     gifOutput.appendChild(img);
   };
-  gif.on("finished", on_finished);
-
-  var shieldImg = document.getElementById("shield-img");
+  gif.on("finished", onFinished);
 
   var date = new Date();
   var dateString = date.toDateString();
@@ -405,48 +388,27 @@ function generateGif(frameManager, isCorrect) {
   var exerciseNumber = document.getElementById("exercise-number").value;
   localStorage.exerciseNumber = exerciseNumber;
 
-  var draw_frame = function(frame, index, array) {
+  board.prepare();
+  board.setContext({
+    isCorrect: isCorrect,
+    dateString: dateString,
+    timeString: timeString,
+    exerciseNumber: exerciseNumber,
+    name: name
+  });
+
+  var drawFrame = function(frame, index, array) {
     ctx.globalAlpha = 1;
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(shieldImg, 0, 0);
-    for (var i = 2; i <= 15; i++) {
-      if (frame.getPinState(i) >= 1) { //if it's on
-        var alpha = (frame.getPinMode(i) === OUTPUT) ? 1 : 0.2; //Make sure it's an output, otherwise dim it
-        alpha *= frame.getPinState(i) / ANALOG_MAX;
-        alpha = Math.max(alpha, 0.3); //Clamp it for usability purposes
-        var radius = 7;
-        var ledDescriptor = ledLookup[i];
-        ctx.globalAlpha = alpha;
-        ctx.fillStyle = ledDescriptor.color;
-        ctx.strokeStyle = ledDescriptor.color;
-        ctx.beginPath();
-        ctx.arc(ledDescriptor.x + radius, ledDescriptor.y + radius, radius, 0, 2 * Math.PI, false);
-        ctx.closePath();
-        ctx.fill();
-      }
-    }
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = "black";
-    ctx.font = "15px monospace";
-    ctx.fillText("Frame: " + index, shieldImg.width + 10, 15);
-    ctx.fillText("PostDelay: " + frame.postDelay, shieldImg.width + 10, 35);
-    ctx.fillText("By " + name, shieldImg.width + 10, 55);
-    ctx.fillText("Exercise " + exerciseNumber, shieldImg.width + 10, 75);
 
-    ctx.fillText(dateString, shieldImg.width + 10, 115);
-    ctx.fillText(timeString, shieldImg.width + 10, 135);
-
-    ctx.font = "bold 15px monospace";
-    ctx.fillStyle = ((typeof(isCorrect) === "undefined") || (isCorrect === false)) ? "red" : "green";
-    var gradeText = (isCorrect === true) ? "Correct" : ((isCorrect === false) ? "Incorrect" : "Ungraded");
-    ctx.fillText(gradeText, shieldImg.width + 10, 175);
+    board.draw(ctx, frame, index, frameManager);
 
     var realDelay = (frame.postDelay === 0) ? 15 : frame.postDelay;
     gif.addFrame(ctx, {copy: true, delay: realDelay});
   };
 
-  frameManager.frames.forEach(draw_frame);
+  frameManager.frames.forEach(drawFrame);
   gif.render();
 
   if ((isCorrect === true) || (isCorrect === false)) {
