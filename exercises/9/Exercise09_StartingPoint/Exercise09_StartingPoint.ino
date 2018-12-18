4eec15b64eef2928e7009f0940494ae3560f64c9
Lyle Harlow <lyle@dpengineering.org>
2018-12-18 05:37:37 -0800

    Updated Exercise 9

---
+++ exercises/9/Exercise09_StartingPoint/Exercise09_StartingPoint.ino
@@ -0,0 +1,34 @@
+void setup()
+{
+  pinMode(2, OUTPUT); // Enables pin 2, 3, 4 on the Arduino to Send enough power to turn on a LED
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  int waitTime; // creates an integer waitTime
+  int count; // creates an integer count
+  waitTime = 400; // sets the value stored in waitTime
+  count = 1; // sets the value stored in count
+
+  while (count <= 4) // this while loop will repeat four times
+  {
+    digitalWrite(2, HIGH);
+    delay(waitTime);
+    digitalWrite(2, LOW);
+    delay(waitTime);
+    count = count + 1; // count gets the value of count + 1
+  }
+
+  count = 1; // resets the value stored in count
+  while (count <= 4) // this while loop will repeat four times
+  {
+    digitalWrite(3, HIGH);
+    delay(waitTime);
+    digitalWrite(3, LOW);
+    delay(waitTime);
+    count = count + 1; // count gets the value of count + 1
+  }
+  delay(waitTime * 10); // wait 4000ms or 4 seconds before going to the next line
+}
