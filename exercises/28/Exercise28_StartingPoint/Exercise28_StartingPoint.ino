ec4f27b7c3041d355bb06443b61345591a53ec47
Lyle Harlow <lyle@dpengineering.org>
2018-12-21 12:56:38 -0800

    Updated Multiple Exercises

---
+++ exercises/28/Exercise28_StartingPoint/Exercise28_StartingPoint.ino
@@ -0,0 +1,24 @@
+void setup() {
+  int ledNumber;
+  for(ledNumber = 2; ledNumber <= 15; ledNumber ++) {
+    pinMode(ledNumber, OUTPUT);
+  }
+}
+
+void loop() {
+  int count;
+  for(count = 1; count <= 4; count++) {
+      digitalWrite(2, HIGH);
+      digitalWrite(9, HIGH);
+      delay(100);
+      digitalWrite(2, LOW);
+      digitalWrite(9, LOW);
+      delay(100);
+      digitalWrite(8, HIGH);
+      digitalWrite(15, HIGH);
+      delay(100);
+      digitalWrite(8, LOW);
+      digitalWrite(15, LOW);
+    } 
+  delay(4000);
+}
