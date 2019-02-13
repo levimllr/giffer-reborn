bfca3a2457acd6dadfec95f161271d69a12160fe
Lyle Harlow <lyle@dpengineering.org>
2019-02-13 06:15:54 -0800

    Initial Commit Ex 46 Start

---
+++ exercises/46/Excercise46_StartingPoint/Excercise46_StartingPoint.ino
@@ -0,0 +1,26 @@
+
+void setup()
+{ 
+  //
+  // configure the LED pins as outputs
+  //
+  for (int ledNumber=2; ledNumber<=15; ledNumber++) // enable all the pins to be outputs
+  {
+    pinMode(ledNumber, OUTPUT);
+  }
+}
+void loop()
+{
+  int myFirstArray[14] = {
+    2,3,4,5,6,7,8,9,10,11,12,13,14,15  }; // creates an array of 14 integers / elements and initializes / assigns values to those integers  
+  int index;
+  // Blinks LED's in the order defined by the array
+  for(index = 0; index <= 14; index++)
+  { 
+    digitalWrite(myFirstArray[index], HIGH);  
+    delay(150);
+    digitalWrite(myFirstArray[index], LOW);
+  }
+  delay(4000);
+}
+
