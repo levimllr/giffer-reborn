a43422ba499f7060aacf075eaaed0d977e64b5b3
Lyle Harlow <lyle@dpengineering.org>
2019-02-21 06:08:13 -0800

    Updated Ex 38 FM

--- exercises/38/Exercise38/Exercise38.ino
+++ exercises/38/Exercise38/Exercise38.ino
@@ -1,4 +1,4 @@
-//**************ACTIVITES TO CHECK WHILE GRADING**********BEGIN*********
+//**************ACTIVITIES TO CHECK WHILE GRADING**********BEGIN*********
 //
 // 1. Ensure that the function is named sequenceLEDsUpAndDown and it is using THREE for loops
 // 2. Verify that LEDs blink in the order 2 and 9, 3 and 10, 4 and 11, 5 and 12, 6 and 13, 7 and 14, 8 and 15, 7 and 14, 6 and 13, 5 and 12, 4 and 11, 3 and 10.
@@ -7,18 +7,18 @@
 // ***************ACTIVITIES TO CHECK WHILE GRADING END*********************
 
 void setup()
-{ 
+{
   int pinNumber;
 
   for(pinNumber = 2; pinNumber <= 15; pinNumber++)
-  { 
+  {
     pinMode(pinNumber, OUTPUT);
   }
 }
 
 void loop()
 {
-  sequenceLEDsUpAndDown(100, 100, 3); 
+  sequenceLEDsUpAndDown(100, 100, 3);
 }
 
 void sequenceLEDsUpAndDown(int onTime, int offTime, int repeatCount)// Exercise19()
@@ -26,12 +26,12 @@ void sequenceLEDsUpAndDown(int onTime, int offTime, int repeatCount)// Exercise1
   int pinNumber;
   int otherpinNumber;
   int count;
-  
+
   for (count = 1; count <= repeatCount; count++)
   {
     for (pinNumber = 2; pinNumber <= 8; pinNumber++)
     {
-      otherpinNumber = pinNumber + 7; 
+      otherpinNumber = pinNumber + 7;
       digitalWrite(pinNumber, HIGH);
       digitalWrite(otherpinNumber, HIGH);
       delay(onTime);
@@ -41,7 +41,7 @@ void sequenceLEDsUpAndDown(int onTime, int offTime, int repeatCount)// Exercise1
     }
     for (pinNumber = 7; pinNumber >= 2; pinNumber--)
     {
-      otherpinNumber = pinNumber + 7; 
+      otherpinNumber = pinNumber + 7;
       digitalWrite(pinNumber, HIGH);
       digitalWrite(otherpinNumber, HIGH);
       delay(onTime);
@@ -51,4 +51,3 @@ void sequenceLEDsUpAndDown(int onTime, int offTime, int repeatCount)// Exercise1
     }
   }
 }
-
