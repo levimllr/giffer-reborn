5e9dbd8c62dc8e6c21f4fe68f6b86e2153715165
Lyle Harlow <lyle@dpengineering.org>
2019-03-04 06:18:51 -0800

    Updated 53 - 54

--- exercises/53/Exercise53/Exercise53.ino
+++ exercises/53/Exercise53/Exercise53.ino
@@ -1,3 +1,10 @@
+//**************ACTIVITIES TO CHECK WHILE GRADING**********BEGIN*********
+//
+// 1.	Giffer checks all aspects of this exercise.
+// 2.   If Giffer says CORRECT it is good to go
+//
+// ***************ACTIVITIES TO CHECK WHILE GRADING END*********************
+
 void setup()
 {
   Serial.begin(9600); // Setup the serial port speed
