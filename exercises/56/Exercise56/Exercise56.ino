c4a86bee9b9cb449aeca38016fb92e7896afd6eb
Lyle Harlow <lyle@dpengineering.org>
2019-03-06 05:09:26 -0800

    Updated Ex 56

--- exercises/56/Exercise56/Exercise56.ino
+++ exercises/56/Exercise56/Exercise56.ino
@@ -88,7 +88,7 @@ void loop()
       if ( potentiometerValueA5 >= 764 && potentiometerValueA5 <= 1023 ) // IF the potentiometerValueA5 is between 764 and 1023
       {
         // Send status to Serial Monitor
-        Serial.print("Onboard POTENTIOMETER -  Pin 5 (0-1023): ");
+        Serial.print("Onboard POTENTIOMETER -  Pin A5 (0-1023): ");
         Serial.println(potentiometerValueA5); // Send the value read from Pin 5: 0 to 1023
         Serial.print("Blink LED 14 and 15");
         Serial.println("");
--- exercises/58/Excercise58_StartingPoint/Excercise58_StartingPoint.ino
+++ exercises/58/Excercise58_StartingPoint/Excercise58_StartingPoint.ino
@@ -1,58 +1,14 @@
-void setup()
-{
-  Serial.begin(9600); // Open serial monitor at 9600 baud to see potentiometer values
-  pinMode(5, INPUT);      // sets the Analog pin 5 as input
-  for (int ledNumber = 2; ledNumber <= 15; ledNumber++)
-  {
-    pinMode(ledNumber, OUTPUT);
-  }
-}
-
-void loop()
-{
-  int potentiometerValueA5;
-  Serial.println("Hello World!");
-  int i = 1;
-  while (i <= 100)
-  {
-    potentiometerValueA5 = analogRead(5); // read the input pin between 0 and 1023
-    // Light Up LED's corresponding to analog values.
-    // If we divide 1024 into 2 areas
-
-    // 000 - 511
-    if ( potentiometerValueA5 >= 0 && potentiometerValueA5 <= 511 )
-    {
-      // Send status to Serial Monitor
-      Serial.print("Onboard POTENTIOMETER -  Pin 5 (0-1023): ");
-      Serial.print(potentiometerValueA5); // Send the value read from Pin 5: 0 to 1023
-      Serial.print("         Blink LED 2 and 3");
-      Serial.println("");
-      digitalWrite(2,HIGH);
-      digitalWrite(3,HIGH);
-      delay(50);
-      digitalWrite(2,LOW);
-      digitalWrite(3,LOW);
-      delay(50);
-    }
-
-    // 512 - 1023
-    if ( potentiometerValueA5 >= 512 && potentiometerValueA5 <= 1023 )
-    {
-      // Send status to Serial Monitor
-      Serial.print("Onboard POTENTIOMETER -  Pin 5 (0-1023): ");
-      Serial.print(potentiometerValueA5); // Send the value read from Pin 5: 0 to 1023
-      Serial.print("         Blink LED 14 and 15");
-      Serial.println("");
-      digitalWrite(14,HIGH);
-      digitalWrite(15,HIGH);
-      delay(50);
-      digitalWrite(14,LOW);
-      digitalWrite(15,LOW);
-      delay(50);
-    }
-    i = i + 1;
-    delay(10);
-  }
-  Serial.println("Good Bye World");
-  delay(10);
-}
+      if ( potentiometerValueA5 >= 512 && potentiometerValueA5 <= 763 ) // IF the potentiometerValueA5 is between 512 and 763
+      {
+        // Send status to Serial Monitor
+        Serial.print("Onboard POTENTIOMETER -  Pin A5 (0-1023): ");
+        Serial.println(potentiometerValueA5); // Send the value read from Pin 5: 0 to 1023
+        Serial.print("Blink LED 9 and 10");
+        Serial.println("");
+        digitalWrite(9, HIGH);
+        digitalWrite(10, HIGH);
+        delay(50);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        delay(50);
+      }
